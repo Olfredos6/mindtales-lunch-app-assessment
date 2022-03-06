716999d4893488823b901f2dc44cc0b12fd2c383
olfredos6 <alfred.nehemie@gmail.com>
2022-03-07 01:53:25 +0200

    adds restaurants models

--- services/restaurants/src/core/models.py
+++ services/restaurants/src/core/models.py
@@ -1,3 +1,52 @@
-# from django.db import models
+from django.db import models
+import uuid
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    date_created: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True
+    )
+    id: models.UUIDField = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name: models.CharField = models.CharField(max_length=150)
+
+
+class Menu(models.Model):
+    # The date_created field below also represents the date of the menu
+    date_created: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True
+    )
+    id: models.UUIDField = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+
+class MenuItem(models.Model):
+    '''
+        Represents an item meal or drink
+        belonging to a menu
+    '''
+    id: models.UUIDField = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    menu: models.ForeignKey = models.ForeignKey(
+        Menu,
+        related_name='menu_item',
+        on_delete=models.CASCADE,
+        null=False
+    )
+    title: models.CharField = models.CharField(
+        max_length=100,
+        null=False
+    )
+    description: models.CharField = models.CharField(
+        max_length=250,
+        null=False
+    )
