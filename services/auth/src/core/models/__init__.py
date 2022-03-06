78ab36aee1fbdc659c42a123c0cc2ad07d31d5e9
olfredos6 <alfred.nehemie@gmail.com>
2022-03-06 23:58:58 +0200

    adds excludes for Flake8 and MyPy

--- services/auth/src/core/models/__init__.py
+++ services/auth/src/core/models/__init__.py
@@ -1,1 +1,2 @@
-from .user import User  # noqa: F401
+from .user import User
+from .other_user_types import Employee, RestaurantManager
--- services/auth/src/core/models/other_user_types.py
+++ services/auth/src/core/models/other_user_types.py
@@ -1,6 +1,4 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
 
 
 User = get_user_model()
@@ -12,11 +10,7 @@ class Employee(User):  # type: ignore
         Different from a RestaturantManager who is
         not considered staff.
     '''
-    is_staff: models.BooleanField = models.BooleanField(
-        _("staff status"),
-        default=True,
-        help_text=_("Designates whether the user is an employee"),
-    )
+    is_staff = True
 
 
 class RestaurantManager(User):  # type: ignore
